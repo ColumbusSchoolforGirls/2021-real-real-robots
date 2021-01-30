1b31c372a64f0135622f3dc91d46b3b5587aacd0
Lina Jeffers <ljeffers@columbusschoolforgirls.org>
2021-01-30 13:36:36 -0500

    Adds global

---
+++ 2021-real-real-robots-master/src/main/java/frc/robot/Global.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+public class Global {
+    //code for deadzone
+    public static final double DEADZONE = 0.17;
+
+    //code for buttons (we don't use all but just in case for later?)
+    public static final int JoystickDrive = 0;
+    public static final int JoystickAux= 1;
+    public static final int BUTTON_A = 1;
+    public static final int BUTTON_B = 2;
+    public static final int BUTTON_X = 3;
+    public static final int BUTTON_Y = 4;
+    public static final int LEFT_BUMPER = 5;
+    public static final int RIGHT_BUMPER = 6;
+    public static final int SELECT = 7;
+    public static final int START = 8;
+    
+}
