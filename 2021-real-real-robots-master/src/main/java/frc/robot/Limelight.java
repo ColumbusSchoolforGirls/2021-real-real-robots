3988ada4a19bcbe338c345a371c9b72e75c4e157
Lina Jeffers <ljeffers@columbusschoolforgirls.org>
2021-02-06 11:01:03 -0500

    test push

--- 2021-real-real-robots-master/src/main/java/frc/robot/Limelight.java
+++ 2021-real-real-robots-master/src/main/java/frc/robot/Limelight.java
@@ -7,14 +7,25 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public final class Limelight {
     NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-    NetworkTableEntry tx = table.getEntry("tx");
-    NetworkTableEntry ty = table.getEntry("ty");
-    NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTableEntry tx = table.getEntry("tx");//horizontal offset from crosshair to target from -29.8 to 29.8 degrees
+    NetworkTableEntry ty = table.getEntry("ty");//vertical offset from crosshair to target from -24.85 to 24.85 degrees
+    //NetworkTableEntry ta = table.getEntry("ta");
+    NetworkTableEntry tv = table.getEntry("tv");//if there is a target, either 0 or 1, 1 means target
     
-    //read values periodically
     double x = tx.getDouble(0.0);
     double y = ty.getDouble(0.0);
-    double area = ta.getDouble(0.0);
+    boolean v = tv.getBoolean(false);
+    //methods for idkkkkk
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public boolean getV(){
+        return v;
+    }
+    //double area = ta.getDouble(0.0);
     
     
     public void teleopPeriodic() {
